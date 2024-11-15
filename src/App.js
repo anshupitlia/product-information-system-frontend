21cb8f69f198782703534e3d9ef7808e197bb6c6
Anshu Pitlia <anshupitlia@gmail.com>
2024-11-15 12:08:34 +0530

    Initial commit with making calls to products and display them in a very basic format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/products');
+    const body = await response.json();
+    this.setState({products: body});
+  }
+
+  render() {
+    const {products} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Products</h2>
+              {products.map(product =>
+                  <div key={product.sku}>
+                    {product.title} ({product.price})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
